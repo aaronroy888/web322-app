3caab1275caaacb7c382d203884ddfa21478f64d
Aaron RoyAlappat <aroyalappat@myseneca.ca>
2025-02-06 14:49:25 -0500

    Updates

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-const express = require('express'); // "require" the Express module
-const app = express(); // obtain the "app" object
-const HTTP_PORT = process.env.PORT || 8080; // assign a port
+const express = require("express");
+const path = require("path");
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express is working!');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.redirect("/about");
 });
 
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "about.html"));
 });
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-
-  app.get('/about', (req, res) => {
-    res.send('About the Company');
-  });
-// start the server on the port and output a confirmation to the console
-app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+app.listen(PORT, () => {
+    console.log(`Express http server listening on port ${PORT}`);
+});
